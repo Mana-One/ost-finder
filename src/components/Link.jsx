c188a479b50b87c1defee2bc08ead7e58f588c8f
Mana-One <paolo.manaois@yahoo.fr>
2021-01-08 11:28:51 +0100

    created Link component

---
+++ src/components/Link.jsx
@@ -0,0 +1,17 @@
+export default Link = ({ className, href, children }) => {
+    const onClick = event => {
+        if( event.metaKey || event.ctrlKey ){
+            return;
+        }
+
+        event.preventDefault();
+        window.history.pushState({}, "", href );
+        window.dispatchEvent( new PopStateEvent( "popstate" ));
+    };
+
+    return( 
+    <a className={className} href={href} onClick={onClick}>
+        {children}
+    </a>
+    );
+};
