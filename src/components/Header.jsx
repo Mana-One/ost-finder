95d21f2e1ca6415cdd6341dd0e90ee5d42425323
Mana-One <paolo.manaois@yahoo.fr>
2021-01-11 19:05:41 +0100

    renamed header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     return(
         <div className="header">
             <Link className="header-link" href="/">Home</Link>
-            <Link className="header-link" href="/api1">Api 1</Link>
-            <Link className="header-link" href="/api2">Api 2</Link>
-            <Link className="header-link" href="/both">Both APIs</Link>
+            <Link className="header-link" href="/api1">Anime Finder</Link>
+            <Link className="header-link" href="/api2">Shazam API</Link>
+            <Link className="header-link" href="/both">OST Finder</Link>
         </div>
     );
 }
