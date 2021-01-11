3a37b67e5a981e0d47c016b14a311fd88f6018ee
Mana-One <paolo.manaois@yahoo.fr>
2021-01-11 10:23:15 +0100

    added Page2 content

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import { ShazamService } from "../services";
+import MusicSearchBar from "./MusicSearchBar.jsx";
 
 const Page2 = () => {
+    const [ musicTitle, setMusicTitle ] = useState("");
+    const [ tracks, setTracks ] = useState([]);
+
+    const clickHandler = async title => {
+        try {
+            if( title !== "" ){
+                const res = await ShazamService.fetchMusic( title );
+                setTracks( res.tracks.hits.map( obj => obj.track ));
+            } else {
+                setTracks([]);
+            }
+
+        } catch( err ){
+            console.log( err.message );
+        }
+    }
+
+    const showList = () => {
+        if( tracks.length ){
+            console.log( tracks );
+            return(
+                <div>
+                    <ul>
+                        {tracks.map( track => <li key={track.key}>Title: {track.title}</li> )}
+                    </ul>
+                </div>
+            );
+        } else {
+            return( <h2>No tracks found</h2> );
+        }
+    }
+
     return(
-        <h1>Page 2</h1>
+        <div className="">
+            <h1>Find music</h1>
+            <MusicSearchBar title={musicTitle} setMusicTitle={setMusicTitle} loadData={clickHandler}/>
+            {showList()}
+        </div>
     );
 }
 
--- src/services/shazam.js
+++ src/services/shazam.js
@@ -1,11 +1,11 @@
 const apiKey = "fea019b2f1mshc64271dd9a4aa30p104fb6jsnddb524717543";
 const shazamUrl = "https://shazam.p.rapidapi.com/search";
 
-const fetchMusic = async title => {
-    const res = await fetch( `${shazamUrl}?offset=0&limit=10&term=${search}`, {
+export const fetchMusic = async title => {
+    const res = await fetch( `${shazamUrl}?offset=0&limit=10&term=${title}`, {
         method: "GET",
         headers: {
-            "x-rapidapi-key": "fea019b2f1mshc64271dd9a4aa30p104fb6jsnddb524717543",
+            "x-rapidapi-key": apiKey,
             "x-rapidapi-host": "shazam.p.rapidapi.com"
         }
     });
